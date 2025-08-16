ef190cb7da22abc33b2ee2465a878cf306d86229
Ross Alexander <84838568+ross8alex@users.noreply.github.com>
2025-08-16 01:52:18 -0400

    Update HTEObservable.UIKit.swift

--- Sources/HighlightedTextEditor/HTEObservable.UIKit.swift
+++ Sources/HighlightedTextEditor/HTEObservable.UIKit.swift
@@ -14,7 +14,7 @@ import UIKit
 public struct HighlightedTextEditorObservable: UIViewRepresentable, HighlightingTextEditorObservable {
     
     var model: HighlightedTextModel
-    var position: Int
+    @Binding var position: Int = 0
     
     public struct Internals {
         public let textView: SystemTextView
